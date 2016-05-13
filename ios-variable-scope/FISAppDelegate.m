772890dcd1bc837c4e393668352c973e8e661a95
saraahmad1114 <saraahmad1114@yahoo.com>
2016-05-13 17:22:33 -0400

    capital letters

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -25,4 +25,28 @@
  
  */
 
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array{
+    NSMutableArray *newArray = [array mutableCopy];
+    [newArray addObject:string];
+    return newArray;
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array;{
+    
+    NSUInteger amount = 0;
+    
+    for (NSUInteger i = 0; i < [array count]; i++){
+        if ([[array[i]uppercaseString] isEqualToString: array[i]]) {
+            amount =amount + 1;
+        }
+        }
+    
+    return amount;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array;{
+    [array removeAllObjects]; 
+}
+
+
 @end
